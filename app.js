138b593f35bbef2edc3309478e879f3812568841
Barasov-Mehdi <barasovmehdii@gmail.com>
2024-07-10 01:37:18 +0400

    10.7.2024

--- app.js
+++ app.js
@@ -36,8 +36,7 @@ app.use('/err', errRouter);
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    
     });
     console.log('MongoDB connected');
   } catch (err) {
